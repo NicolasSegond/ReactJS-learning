0c0a726fbda46ce03fa0c6a32769606e4e384943
TurboNicoo <nicolassegond0@gmail.com>
2023-07-23 15:15:08 +0200

    Advancement

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+/*const name = 'Nicolas Segond';
+let tabIndex = 2;
+
+const element = <div>
+                  <h1 tabIndex={tabIndex}>Bienvenue les étudiants !</h1>
+                  <p> Vous êtes en forme ? </p>
+                  <p> Je m'appelle {name} </p>
+                </div>*/
+
+function FormatUser(props){
+  return props.firstName + ' ' + props.lastName
+}
+
+const user = {
+  firstName : 'Nicolas',
+  lastName : 'Segond'
+}
+
+class FormatUserV2 extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return this.props.firstName + ' ' + this.props.lastName + ' il s\'agit de la version V2'
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<FormatUserV2 firstName={user.firstName} lastName={user.lastName}/>);
