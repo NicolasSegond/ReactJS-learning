9bdbecbdff50b83ea3a6be443191859b2f1ec266
TurboNicoo <nicolassegond0@gmail.com>
2023-07-24 16:40:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const element = <div>
                   <h1 tabIndex={tabIndex}>Bienvenue les étudiants !</h1>
                   <p> Vous êtes en forme ? </p>
                   <p> Je m'appelle {name} </p>
-                </div>*/
+                </div>
 
 function FormatUser(props){
   return props.firstName + ' ' + props.lastName
@@ -32,3 +32,71 @@ class FormatUserV2 extends React.Component{
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<FormatUserV2 firstName={user.firstName} lastName={user.lastName}/>);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Heure(){
+  return <h1>il est {new Date().toLocaleTimeString()}</h1>
+}
+
+function tick(){
+  root.render(<Heure/>);
+}
+
+class Heure extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {date: new Date()}
+  }
+
+  componentDidMount(){
+    this.timerId = setInterval(() => {
+      this.setState({date : new Date()})
+    },1000)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerId)
+  }
+
+  tick(){
+
+  }
+
+  render(){
+    return <h1>il est {this.state.date.toLocaleTimeString()}</h1>
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Heure />);*/
+
+class Increment extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {count : 0}
+    this.handleIncrement = this.handleIncrement.bind(this)
+    this.handleDecrement = this.handleDecrement.bind(this)
+  }
+
+  handleIncrement(){
+    this.setState({count : this.state.count+1})
+  }
+
+  handleDecrement(){
+    this.setState({count : this.state.count-1})
+  }
+
+  render(){
+    return <div>
+            <p>La valeur est : {this.state.count} </p>
+            <button type='button' onClick={this.handleIncrement}> Increment </button>
+            <button type='button' onClick={this.handleDecrement}> Increment </button>
+           </div>
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Increment />);
