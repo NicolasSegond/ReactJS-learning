68a55d4567b2b2a07c5a07e434d2fa7c24bd856f
TurboNicoo <nicolassegond0@gmail.com>
2023-07-26 20:50:44 +0200

    end of udemy training

--- src/index.js
+++ src/index.js
@@ -70,33 +70,87 @@ class Heure extends React.Component{
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Heure />);*/
+root.render(<Heure />);
 
 class Increment extends React.Component{
   constructor(props){
     super(props)
 
-    this.state = {count : 0}
+    this.state = {count : 0, showDecrement : false}
     this.handleIncrement = this.handleIncrement.bind(this)
     this.handleDecrement = this.handleDecrement.bind(this)
   }
 
   handleIncrement(){
-    this.setState({count : this.state.count+1})
+    if (this.state.count == 9){
+      this.setState({count : this.state.count+1, showDecrement : true})
+    } else{
+      this.setState({count : this.state.count+1, showDecrement : false})
+    }
   }
 
   handleDecrement(){
-    this.setState({count : this.state.count-1})
+    if(this.state.count > 1){
+      this.setState({count : this.state.count-1})
+    } else{
+      this.setState({count : this.state.count-1, showDecrement : false})
+    }
   }
 
   render(){
     return <div>
             <p>La valeur est : {this.state.count} </p>
-            <button type='button' onClick={this.handleIncrement}> Increment </button>
-            <button type='button' onClick={this.handleDecrement}> Increment </button>
+            {this.state.showDecrement ? <button type='button' onClick={this.handleDecrement}> Descrement </button> : 
+            <button type='button' onClick={this.handleIncrement}> Increment </button>}
            </div>
   }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Increment />);
+
+class Listes extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return <ul>{this.props.values.map((value) => 
+      <li key={value.id}>{value.name}</li>
+    ) }</ul>
+  }
+}
+
+const nombre = [1, 2, 3, 4, 5]
+
+const persons = [
+  {id: 1, name: 'Brice'},
+  {id: 2, name: 'Herve'}
+]
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Listes values = {persons} />);*/
+
+class Inputs extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = { nom : ""}
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e){
+    console.log(e.target)
+    this.setState({nom: e.target.value})
+  }
+
+  render(){
+    return <div>
+            <label>Nom</label>
+            <input type='text' name='nom' value={this.state.nom} onChange={this.handleChange}></input>
+            <p>{this.state.nom}</p>
+           </div>
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Inputs />);
